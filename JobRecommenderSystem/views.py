3d1179872eb0a452b6ba79ddac4b62dd92edd508
jamshaid <jamshaidsohail@gmail.com>
2017-11-17 22:17:34 +0500

    Initial Commit

--- JobRecommenderSystem/views.py
+++ JobRecommenderSystem/views.py
@@ -1,14 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-from Models.models import Poll
-
-
-def Signup(request):
-    return render(request, 'Views/../accounts/template/Signupform.html')
-
-
-def Signin(request):
-    return render(request, 'Views/../accounts/template/Signinform.html')
+def home (request):
+    return render(request, 'accounts/Signupform.html')
 
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
 
 
+def signup(request):
+    return render(request, 'accounts/Signupform.html')
+
+
+def loginview(request):
+    return render(request, 'accounts/Signinform.html')
+
+
+def logoutview(request):
+    return render(request,'accounts/Signupform.html')
