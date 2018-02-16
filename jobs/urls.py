a75dc679228978621fbd859f170a74a0acdce879
jamshaid <jamshaidsohail@gmail.com>
2018-02-16 22:15:36 +0500

    Initial Commit

--- jobs/urls.py
+++ jobs/urls.py
@@ -5,5 +5,6 @@ app_name = 'jobs'
 
 urlpatterns = [
     url(r'^$', views.jobsviewing, name="jobs"),
-    url(r'^actualjobs/',views.jobsretrieving,name="jobsretrieve")
+    url(r'^actualjobs/',views.jobsretrieving,name="jobsretrieve"),
+    url(r'^jobsDetails/',views.displayingJobDetail,name = "jobsdisplay")
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -9,8 +9,8 @@ import re
 
 class Jobs(object):
     # Note that we're taking an argument besides self, here.
-    def __init__(self,id = 0,jobTitle = "",jobCompany = "",jobLocation = "",jobSalary = "",jobSummary = ""):
-        self.id = id  # Set the room's name to the name we got.
+    def __init__(self, id=0, jobTitle="", jobCompany="", jobLocation="", jobSalary="", jobSummary=""):
+        self.id = id
         self.jobTitle = jobTitle
         self.jobCompany = jobCompany
         self.jobLocation = jobLocation
@@ -18,9 +18,27 @@ class Jobs(object):
         self.jobSummary = jobSummary
 
 
-
 def jobsviewing(request):
     return render(request, 'jobs.html')
+
+
+def displayingJobDetail(request):
+    if request.method == "POST":
+        titleName = [key for key in request.POST if key.startswith("Titlename")]
+        companyName = [key for key in request.POST if key.startswith("CompanyName")]
+        jobId = [key for key in request.POST if key.startswith("jobId")]
+        jobLocation = [key for key in request.POST if key.startswith("jobLocation")]
+        jobSalary = [key for key in request.POST if key.startswith("jobSalary")]
+        jobDescription = [key for key in request.POST if key.startswith("jobDescription")]
+
+        print("It enters the function again")
+        print(titleName)
+        print(companyName)
+        print(jobId)
+        print(jobLocation)
+        print(jobSalary)
+        print(jobDescription)
+        return render(request, 'jobsDetail.html')
 
 
 def jobsretrieving(request):
@@ -35,16 +53,16 @@ def jobsretrieving(request):
 
         print('4')
 
-        #keyword = 'python'
-        #location = 'lahore'
+        # keyword = 'python'
+        # location = 'lahore'
 
-        #client = MongoClient('localhost:27017')
-        #db = client.JobDatabase
+        # client = MongoClient('localhost:27017')
+        # db = client.JobDatabase
 
-        #desc = "web"
-        #loc = "lahore"
+        # desc = "web"
+        # loc = "lahore"
         r = requests.get("https://www.indeed.com/jobs?q=" + keyword + "&l=" + location,
-                         proxies={"http": "http://61.233.25.166:80"})
+                         proxies={"http": "http://35.196.26.166:3128"})
         data = r.text
         soup = BeautifulSoup(data, 'lxml')
         soup = soup.findAll("a", {"class": "turnstileLink"})
@@ -64,12 +82,11 @@ def jobsretrieving(request):
                 #     {
                 #         "ID": j,
                 #     })
-                print ("ID :",j)
+                print("ID :", j)
                 id = j
 
-
                 jobRequest = requests.get("https://www.indeed.com" + jobLink,
-                                          proxies={"http": "http://61.233.25.166:80"})
+                                          proxies={"http": "http://35.196.26.166:3128"})
                 jobData = jobRequest.text
                 jobSoup = BeautifulSoup(jobData, 'lxml')
 
@@ -79,33 +96,27 @@ def jobsretrieving(request):
                 #     {"ID": j},
                 #     {"$set": {"Title": jobTitle.text}}
                 # )
-                print("Title:",jobTitle.text)
+                print("Title:", jobTitle.text)
 
                 title = jobTitle.text
-
-
-
 
                 jobCompany = jobSoup.find("span", {"class": "company"})
                 if jobCompany:
                     company = jobCompany.text
-                    print("Company:",jobCompany.text)
+                    print("Company:", jobCompany.text)
                     # db.Jobs.update(
                     #     {"ID": j},
                     #     {"$set": {"Company": jobCompany.text}}
                     # )
 
-
-
                 jobLocation = jobSoup.find("span", {"class": "location"})
                 if jobLocation:
                     location = jobLocation.text
-                    print("Location:",jobLocation.text)
+                    print("Location:", jobLocation.text)
                     # db.Jobs.update(
                     #     {"ID": j},
                     #     {"$set": {"Location": jobLocation.text}}
                     # )
-
 
                 jobSalary = jobSoup.find("span", {"class": "no-wrap"})
 
@@ -115,23 +126,25 @@ def jobsretrieving(request):
                     #     {"ID": j},
                     #     {"$set": {"Salary": jobSalary.text}}
                     # )
-                    print("Salary:",jobSalary.text.lstrip())
+                    print("Salary:", jobSalary.text.lstrip())
 
                 jobSummary = jobSoup.find("span", {"class": "summary"})
                 if jobSummary:
                     summary = jobSummary.text.encode('utf-8')
-                    print("Summary:",jobSummary.text.encode('utf-8'))
+                    print("Summary:", jobSummary.text.encode('utf-8'))
                     # db.Jobs.update(
                     #     {"ID": j},
                     #     {"$set": {"Summary": jobSummary.text}}
                     # )
                 j += 1
-                jobs.append(Jobs(id,title,company,location,salary,summary))
+                jobs.append(Jobs(id, title, company, location, salary, summary))
 
             if j == 4:
                 break
 
         return render(request, 'jobs.html', {"jobList": jobs})
+    else:
+        return render(request, 'jobs.html')
 
 
 
@@ -140,37 +153,35 @@ def jobsretrieving(request):
 
 
 
-
-                # result_json = cj.search({
-                #     'location': location,
-                #     'keywords': keyword,
-                #     'affid': '213e213hd12344552',
-                #     'user_ip': '11.22.33.44',
-                #     'url': 'http://www.example.com/jobsearch?q=python&l=london',
-                #     'user_agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0'
-                # });
-                # print('5')
-                #
-                #
-                # now = datetime.now()
-                # print('6')
-                #
-                # for job in result_json["jobs"]:
-                #     jobPosted = job["date"]
-                #     jobPosted = datetime.strptime(jobPosted, '%a, %d %b %Y %H:%M:%S %Z')
-                #     job["dayMonth"] = str(jobPosted.strftime('%b %d'))
-                #     if abs((now - jobPosted).days) == 1:
-                #         job["date"] = str(abs((now - jobPosted).days)) + " day"
-                #     elif abs((now - jobPosted).days) == 0:
-                #         job["date"] = str(abs((now - jobPosted).seconds) / 3600) + " hours"
-                #     else:
-                #         job["date"] = str(abs((now - jobPosted).days)) + " days"
-                #         print("7")
-                #
-                # print("8")
-                # i = 0;
-                # for job in result_json["jobs"]:
-                #     job["description"] = job["description"].replace('</b>', '').replace('<b>', '').split()
-                #     job["id"] = i
-                #     i = i + 1
-
+        # result_json = cj.search({
+        #     'location': location,
+        #     'keywords': keyword,
+        #     'affid': '213e213hd12344552',
+        #     'user_ip': '11.22.33.44',
+        #     'url': 'http://www.example.com/jobsearch?q=python&l=london',
+        #     'user_agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0'
+        # });
+        # print('5')
+        #
+        #
+        # now = datetime.now()
+        # print('6')
+        #
+        # for job in result_json["jobs"]:
+        #     jobPosted = job["date"]
+        #     jobPosted = datetime.strptime(jobPosted, '%a, %d %b %Y %H:%M:%S %Z')
+        #     job["dayMonth"] = str(jobPosted.strftime('%b %d'))
+        #     if abs((now - jobPosted).days) == 1:
+        #         job["date"] = str(abs((now - jobPosted).days)) + " day"
+        #     elif abs((now - jobPosted).days) == 0:
+        #         job["date"] = str(abs((now - jobPosted).seconds) / 3600) + " hours"
+        #     else:
+        #         job["date"] = str(abs((now - jobPosted).days)) + " days"
+        #         print("7")
+        #
+        # print("8")
+        # i = 0;
+        # for job in result_json["jobs"]:
+        #     job["description"] = job["description"].replace('</b>', '').replace('<b>', '').split()
+        #     job["id"] = i
+        #     i = i + 1
